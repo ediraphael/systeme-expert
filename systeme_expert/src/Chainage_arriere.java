c31ffcec6578ffac79e742e4c4a9d65245ef7b1b
yassine.badih@gmail.com <yassine.badih@gmail.com@3e58e296-7274-44fe-15e5-75ec72d03039>
2013-10-14 16:26:34 +0000

    Update chainage arrière

--- systeme_expert/src/Chainage_arriere.java
+++ systeme_expert/src/Chainage_arriere.java
@@ -1,15 +1,55 @@
+import java.util.Vector;
 
-public class Chainage_arriere extends Chainage_abstrait
-{
-	public Chainage_arriere(Base_de_regle regles, Base_de_fait faits)
-	{
+public class Chainage_arriere extends Chainage_abstrait {
+	public Chainage_arriere(Base_de_regle regles, Base_de_fait faits) {
 		super(regles, faits);
 	}
 
-	public void evaluer()
-	{
-		System.out.println("totot");
-		// TODO Auto-generated method stub
-		
-	}	
+	public void evaluer() {
+		for (Litteral litteral : Liste_litteral.values()) {
+			if (evaluerArriere(litteral)) {
+				this.getFaits().getFaits().add(litteral);
+			}
+		}
+
+	}
+
+	public boolean evaluerArriere(Litteral b) {
+		boolean dem = false;
+
+		// Premier cas evident
+		for (Litteral flitteral : this.getFaits().getFaits()) {
+			if (b.toString().equals(flitteral.toString()))
+				dem = true;
+		}
+
+		// 2eme cas: rechercher si litteral deductible a partir de BR U BF
+		for (Regle regle : this.getRegles().getRegles()) {
+			for (Litteral r : regle.getConclusion()) {
+				if (r.toString().equals(b.toString())) {
+					while (dem == false) {
+						dem = Verif(regle.getCondition(), this.getFaits());
+					}
+				}
+			}
+		}
+
+		// 3eme cas: sinon voir si b est demandable
+
+		// A faire
+
+		return dem;
+	}
+
+	public boolean Verif(Vector<Litteral> condition, Base_de_fait BF) {
+		boolean ver = true;
+
+		for (Litteral b : BF.getFaits()) {
+			while (ver) {
+				ver = evaluerArriere(b);
+			}
+		}
+		return ver;
+	}
+
 }
